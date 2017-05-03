17cf731bcde798bb3ea7cff656e5008c1784f8ac
Ykkris <nemoselache@gmail.com>
2017-05-03 20:47:17 +0200

    salaire médecin / flics

--- resources/paycheck/paycheck_sv.lua
+++ resources/paycheck/paycheck_sv.lua
@@ -16,7 +16,7 @@ AddEventHandler('paycheck:salary', function()
     if job == 2 then
       user:addMoney(salary_job)
       TriggerClientEvent("es_freeroam:notify", source, "CHAR_BANK_MAZE", 1, "Maze Bank", false, "Salaire metier reçu : + "..salary_job.." ~g~€")
-	elseif job == 6 then
+	elseif job == 3 then
 	  user:addMoney(salary_job)
 	  TriggerClientEvent("es_freeroam:notify", source, "CHAR_BANK_MAZE", 1, "Maze Bank", false, "Salaire metier reçu : + "..salary_job.." ~g~€")
     end
