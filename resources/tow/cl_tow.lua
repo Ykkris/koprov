0e711b6ae734ce4422d1598c59f494db0f34ffa0
izio38 <romainbillot3009@gmail.com>
2017-05-06 18:11:51 +0200

    check distance camion / targetveh

--- resources/tow/cl_tow.lua
+++ resources/tow/cl_tow.lua
@@ -1,4 +1,5 @@
 local currentlyTowedVehicle = nil
+local radius = 2.0
 
 RegisterNetEvent('pv:tow')
 AddEventHandler('pv:tow', function()
@@ -14,14 +15,24 @@ AddEventHandler('pv:tow', function()
 		local coordA = GetEntityCoords(playerped, 1)
 		local coordB = GetOffsetFromEntityInWorldCoords(playerped, 0.0, 5.0, 0.0)
 		local targetVehicle = getVehicleInDirection(coordA, coordB)
-		
+
 		if currentlyTowedVehicle == nil then
 			if targetVehicle ~= 0 then
 				if not IsPedInAnyVehicle(playerped, true) then
 					if vehicle ~= targetVehicle then
-						AttachEntityToEntity(targetVehicle, vehicle, 20, -0.5, -5.0, 1.0, 0.0, 0.0, 0.0, false, false, false, false, 20, true)
-						currentlyTowedVehicle = targetVehicle
-						TriggerEvent("chatMessage", "[TOWFuckers]", {255, 255, 0}, "Vehicle successfully attached to towtruck!")
+						local postargetveh = GetEntityCoords(targetVehicle ,1)
+						local posveh = GetEntityCoords(vehicle, 1)
+						local diffpos = GetDistanceBetweenCoords(postargetveh.x, postargetveh.y, postargetveh.z, posveh.x, posveh.y, posveh.z, true)
+						
+						if diffpos <= radius then
+								
+							AttachEntityToEntity(targetVehicle, vehicle, 20, -0.5, -5.0, 1.0, 0.0, 0.0, 0.0, false, false, false, false, 20, true)
+							currentlyTowedVehicle = targetVehicle
+							TriggerEvent("chatMessage", "[TOWFuckers]", {255, 255, 0}, "Vehicle successfully attached to towtruck!")
+								
+						else
+							ShowNotification("Vous n'êtes pas a coté d'un véhicule.")
+						end
 					else
 						TriggerEvent("chatMessage", "[TOWFuckers]", {255, 255, 0}, "Are you retarded? You cant tow your own towtruck with your own towtruck?")
 					end
@@ -44,4 +55,11 @@ function getVehicleInDirection(coordFrom, coordTo)
 	local rayHandle = CastRayPointToPoint(coordFrom.x, coordFrom.y, coordFrom.z, coordTo.x, coordTo.y, coordTo.z, 10, GetPlayerPed(-1), 0)
 	local a, b, c, d, vehicle = GetRaycastResult(rayHandle)
 	return vehicle
+end
+	
+function ShowNotification(message)
+	SetNotificationTextEntry("STRING")
+	AddTextComponentString(message)
+	DrawNotification(false, false)
+end	
 end
