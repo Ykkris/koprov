eda7c82d40f89feb2f4045e9eabb7b5df1cd10c6
Ykkris <nemoselache@gmail.com>
2017-05-07 06:13:28 +0200

    try cotcot

--- resources/cotcot/client.lua
+++ resources/cotcot/client.lua
@@ -1,14 +1,16 @@
-Citizen.CreateThread(function()
-    local pop_finish = false
-    local count = 0
-    if not(pop_finish) then
-      while count > 0 do
-        poulet = CreatePed(28, 1794449327, 2126.68, 5013.56, 40.42, 0.0, false)
-        SetEntityAsMissionEntity(poulet, true, true)
-    
-       pop_finish = true
-      end
+local firstspawn = 0
+local peds = { { 2126.68, 5013.56, 41.42} }
+local thisPed
+local pedCoords = {}
+local storedPeds = {}
+AddEventHandler('playerSpawned', function(spawn)
+  if firstspawn == 0 then
+    for i=1, 10, 1 do
+      pedCoords = peds[i]
+      thisPed = CreatePed(26, 1706635382, pedCoords[1], pedCoords[2], pedCoords[3], 0.0, true, true)
+      table.insert(storedPeds, {ped = thisPed})
+      SetEntityInvincible(thisPed, true)
     end
-    
-    )
-end
+    firstspawn = 1
+  end
+end)
