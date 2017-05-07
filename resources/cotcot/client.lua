d2fd4f75870fb924cbed199d8890c5fe2425d60a
izio38 <romainbillot3009@gmail.com>
2017-05-07 07:37:45 +0200

    Update client.lua

--- resources/cotcot/client.lua
+++ resources/cotcot/client.lua
@@ -10,7 +10,7 @@ Citizen.CreateThread(function()
     local pop_finish = false
     local count = 0
     if not(pop_finish) then
-      while count >= 0 do
+      while count <= 50 do
         poulet = CreatePed(hash, 2126.68, 5013.56, 40.42, 0.0 ,true)
         count = count + 1
       end
