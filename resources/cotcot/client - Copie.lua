43231d30c74afa2d728d44532f441ef872dc7da5
izio38 <romainbillot3009@gmail.com>
2017-05-07 07:22:45 +0200

    Update client - Copie.lua

--- resources/cotcot/client - Copie.lua
+++ resources/cotcot/client - Copie.lua
@@ -1,14 +1,16 @@
 RequestModel( GetHashKey( "A_C_Cow" ) )
+hash = GetHashKey( "A_C_Cow" )
 while ( not HasModelLoaded( GetHashKey( "A_C_Cow" ) ) ) do
     Citizen.Wait( 1 )
 end
+-- CreateVehicle(MISSION.hashTruck, 12.1995, -1.174761, 73.000, 0.0, true, false)
 
 Citizen.CreateThread(function()
     local pop_finish = false
     local count = 0
     if not(pop_finish) then
       while count > 0 do
-        poulet = CreatePed(28, 0xFCFA9E1E, 2126.68, 5013.56, 40.42, 0.0, false)
+        poulet = CreatePed(hash, 2126.68, 5013.56, 40.42, 0.0 ,true)
     
        pop_finish = true
       end
