2d8feb533fb09f01eda1f79d449b79adb460a2e7
izio38 <romainbillot3009@gmail.com>
2017-05-08 01:54:16 +0200

    Update client.lua

--- resources/jobs_system/client.lua
+++ resources/jobs_system/client.lua
@@ -80,13 +80,14 @@ Citizen.CreateThread(function()
       Menu.hidden = not Menu.hidden 
     end
   end
-  --  if (IsControlJustPressed(1,Keys["U"]) then
-  --    Citizen.Trace(tostring(Menu.hidden))
-  --    Citizen.Trace(tostring(IsNearJob))
-  --  end
+    if (IsControlJustPressed(1,Keys["U"]) then
+      Citizen.Trace(tostring(Menu.hidden))
+      Citizen.Trace(tostring(IsNearJob))
+    end
     if (IsNearJobs() == false) and (Menu.hidden == false) then
-          menuJobs()
+          ClearMenu()
           Menu.hidden = not Menu.hidden
+          menuJobs()
     end
     Menu.renderGUI()
   end
