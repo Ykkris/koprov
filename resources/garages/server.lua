555e18cb0cad04473cad60312af24787516f788b
Ykkris <nemoselache@gmail.com>
2017-05-07 14:20:09 +0200

    Revert "Update server.lua"
    This reverts commit 115c744aff87786eaaed214465ee76eae229c00a.

--- resources/garages/server.lua
+++ resources/garages/server.lua
@@ -1,8 +1,8 @@
 require "resources/essentialmode/lib/MySQL"
 MySQL:open("127.0.0.1", "gta5_gamemode_essential", "root", "5M32bNCpFdgG")
 
-RegisterServerEvent('test:SelVeh')
-RegisterServerEvent('test:CheckGarageForVeh')
+RegisterServerEvent('garages:SetVehOut')
+RegisterServerEvent('garages:SetVehIn')
 RegisterServerEvent('garages:GetPlayerVehs')
 
 -- Checks if the plate belongs to the user, if it does it updates the vehicle as in, then send the remove order to the client
@@ -27,45 +27,45 @@ AddEventHandler('garages:SetVehIn', function(plate)
   end)
 end)
 
-
-AddEventHandler('test:CheckGarageForVeh', function()
-  vehicles = {}
-  TriggerEvent('es:getPlayerFromId', source, function(user)
-    local player = user.identifier  
-    local executed_query = MySQL:executeQuery("SELECT * FROM user_vehicle WHERE identifier = '@username'",{['@username'] = player})
-    local result = MySQL:getResults(executed_query, {'id','vehicle_model', 'vehicle_name', 'vehicle_state'}, "id")
-    if (result) then
-        for _, v in ipairs(result) do
-                --print(v.vehicle_model)
-                --print(v.vehicle_plate)
-                --print(v.vehicle_state)
-                --print(v.id)
-            t = { ["id"] = v.id, ["vehicle_model"] = v.vehicle_model, ["vehicle_name"] = v.vehicle_name, ["vehicle_state"] = v.vehicle_state}
-            table.insert(vehicles, tonumber(v.id), t)
-        end
-    end
-  end)  
-    --print(vehicles[1].id)
-    --print(vehicles[2].vehicle_model)
-    TriggerClientEvent('garages:getVehicles', source, vehicles)
-end)
-
-AddEventHandler('test:SelVeh', function(plate)
+-- Updates the vehicle linked to the mentionned plate as out
+AddEventHandler('garages:SetVehOut', function(plate)
+  --print('garages:SetVehOut')
   TriggerEvent('es:getPlayerFromId', source, function(user)
     local player = user.identifier
     local plate = plate
+    local state = "out"
 
-    local executed_query = MySQL:executeQuery("SELECT * FROM user_vehicle WHERE identifier = '@username' AND vehicle_plate ='@plate'",{['@username'] = player, ['@vehicle'] = vehicle, ['@plate'] = plate})
-    local result = MySQL:getResults(executed_query, {'vehicle_price'}, "identifier")
-    if(result)then
-      for k,v in ipairs(result)do
-        price = v.vehicle_price
-      local price = price / 2      
-      user:addMoney((price))
-      end
-    end
-    local executed_query = MySQL:executeQuery("DELETE from user_vehicle WHERE identifier = '@username' AND vehicle_plate = '@plate'",
-      {['@username'] = player, ['@plate'] = plate})
-    TriggerClientEvent("es_freeroam:notify", source, "CHAR_SIMEON", 1, "Simeon", false, "Véhicule vendu!\n")
+    MySQL:executeQuery("UPDATE user_vehicle SET vehicle_state='@state' WHERE identifier = '@username' AND vehicle_plate = '@plate'",
+      {['@username'] = player, ['@plate'] = plate, ['@state'] = state})
   end)
 end)
+
+-- Returns the list of "button" = list of vehicles with all their informations
+AddEventHandler('garages:GetPlayerVehs', function(plate)
+  --print('garages:GetPlayerVehs')
+  TriggerEvent('es:getPlayerFromId', source, function(user)
+    local player = user.identifier
+    local executed_query = MySQL:executeQuery("SELECT * FROM user_vehicle WHERE identifier = '@username' AND vehicle_state = 'in'",{['@username'] = player})
+    local result = MySQL:getResults(executed_query, {'vehicle_name', 'vehicle_model', 'vehicle_plate', 'vehicle_colorprimary', 'vehicle_colorsecondary', 'vehicle_state'}, "identifier")
+    local buttons = {}
+
+    if(result)then
+      for k,v in ipairs(result)do
+
+        local name = v.vehicle_name
+        local model = v.vehicle_model
+        local plate = v.vehicle_plate
+        local primarycolor = v.vehicle_colorprimary
+        local secondarycolor = v.vehicle_colorsecondary
+        local state = v.vehicle_state
+        
+        table.insert(buttons, {name = name, model = model, plate = plate, primarycolor= primarycolor, secondarycolor = secondarycolor, state = state})
+
+      end
+    end
+
+    TriggerClientEvent('garages:DisplayVehicles', source, buttons)
+
+  end)
+end)
+
