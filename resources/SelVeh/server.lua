8517bfe1e571659fcdd138fe2a5d443e5787e897
izio38 <romainbillot3009@gmail.com>
2017-05-07 14:55:03 +0200

    Update server.lua

--- resources/SelVeh/server.lua
+++ resources/SelVeh/server.lua
@@ -24,15 +24,15 @@ AddEventHandler('test:SelVeh', function(plate)
   TriggerEvent('es:getPlayerFromId', source, function(user)
     local player = user.identifier
     local plate = plate
+       Citizen.Trace(tostring(plate))
 
-    local executed_query = MySQL:executeQuery("SELECT * FROM user_vehicle WHERE identifier = '@username' AND vehicle_plate ='@plate'",{['@username'] = player, ['@vehicle'] = vehicle, ['@plate'] = plate})
+    local executed_query = MySQL:executeQuery("SELECT * FROM user_vehicle WHERE identifier = '@username' AND vehicle_plate ='@plate'",{['@username'] = player, ['@plate'] = plate})
     local result = MySQL:getResults(executed_query, {'vehicle_price'}, "identifier")
     if(result)then
-      for k,v in ipairs(result)do
-        price = v.vehicle_price
-      local price = price / 2      
-      user:addMoney((price))
-      end
+        price = result[1].vehicle_price
+       local price = price / 2      
+       user:addMoney((price))
+
     end
     local executed_query = MySQL:executeQuery("DELETE from user_vehicle WHERE identifier = '@username' AND vehicle_plate = '@plate'",
       {['@username'] = player, ['@plate'] = plate})
