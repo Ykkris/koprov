54b96acc15e6662e2c5377b31e7bc614ef279aa8
izio38 <romainbillot3009@gmail.com>
2017-05-07 05:37:28 +0200

    Update client.lua

--- resources/cotcot/client.lua
+++ resources/cotcot/client.lua
@@ -1,2 +1,14 @@
-poulet = CreatePed(28, 1794449327, 2126.68, 5013.56, 40.42, 0.0, false)
-SetEntityAsMissionEntity(poulet, p1, p2)
+Citizen.CreateThread(function()
+    local pop_finish = false
+    local count = 0
+    if not(pop_finish) then
+      while count > 0 do
+        poulet = CreatePed(28, 1794449327, 2126.68, 5013.56, 40.42, 0.0, false)
+        SetEntityAsMissionEntity(poulet, true, true)
+    
+       pop_finish = true
+      end
+    end
+    
+    )
+end
