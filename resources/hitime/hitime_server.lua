908347dcbb02b1c8f128afb3da73a14a187c13f4
izio38 <romainbillot3009@gmail.com>
2017-05-07 17:19:04 +0200

    Update hitime_server.lua

--- resources/hitime/hitime_server.lua
+++ resources/hitime/hitime_server.lua
@@ -7,14 +7,10 @@ actualplayer = 0
 RegisterServerEvent('Join')
 AddEventHandler('Join', function()
 	actualplayer = source
-	RconPrint("source = ".. tostring(source))
 	local players = 0
 	players = GetPlayers()
 	if #players >= 2 then
 		TriggerClientEvent("client.time", players[1])
-		RconPrint("on prend le time de : " .. tostring(players[1]))
-		RconPrint("aui est players 2 : " .. tostring(players[2]))
-		RconPrint("qui est player 3 : " .. tostring(players[3]))
 	end
 
 
@@ -25,7 +21,6 @@ end)
 RegisterServerEvent('return.client.time')
 AddEventHandler('return.client.time', function(hours, minuts, seconds)
 		
-	RconPrint("L heure du return est "..tostring(hours).." : "..tostring(minuts).." : "..tostring(seconds))
 
 	TriggerClientEvent("update.client.time", actualplayer, hours, minuts, seconds)
 	
