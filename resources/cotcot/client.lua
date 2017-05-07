5bceaa4eca69e784bf68b9461cc8c0718853ed2c
izio38 <romainbillot3009@gmail.com>
2017-05-07 07:35:44 +0200

    Update client.lua

--- resources/cotcot/client.lua
+++ resources/cotcot/client.lua
@@ -6,12 +6,11 @@ end
 -- CreateVehicle(MISSION.hashTruck, 12.1995, -1.174761, 73.000, 0.0, true, false)
 
 Citizen.CreateThread(function()
-       Wait(1000)
-       poulet = CreatePed(28, hash, 2126.68, 5013.56, 40.42, 0.0 ,true)
+
     local pop_finish = false
     local count = 0
     if not(pop_finish) then
-      while count > 0 do
+      while count >= 0 do
         poulet = CreatePed(hash, 2126.68, 5013.56, 40.42, 0.0 ,true)
         count = count + 1
       end
