784509ebef765b07f8cad942036842001763719a
izio38 <romainbillot3009@gmail.com>
2017-05-08 03:13:12 +0200

    Update server.lua

--- resources/vdk_recolt/server.lua
+++ resources/vdk_recolt/server.lua
@@ -16,7 +16,7 @@ AddEventHandler("jobs:getJobs", function ()
 		end
 	else
 		jobs = result
-		Citizen.Trace(tostring(jobs))
+		RconPrint(tostring(jobs))
 	end
     TriggerClientEvent("cli:getJobs", source, jobs)
 end)
