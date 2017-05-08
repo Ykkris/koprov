eec99aa97d6caeb8f5303d4d9ad21cfefa3e5e4a
izio38 <romainbillot3009@gmail.com>
2017-05-08 02:53:38 +0200

    Update sv_weashop.lua

--- resources/[essential]/es_weashop/sv_weashop.lua
+++ resources/[essential]/es_weashop/sv_weashop.lua
@@ -55,7 +55,7 @@ AddEventHandler("weaponshop:GiveWeaponsToPlayer", function(player)
 		local executed_query = MySQL:executeQuery("SELECT * FROM user_weapons WHERE identifier = '@username'",{['@username'] = playerID})
 		local result = MySQL:getResults(executed_query, {'weapon_model','withdraw_cost'}, "identifier")
 	
-		delay = 1000
+		delay = 500
 		if(result)then
 			for k,v in ipairs(result) do
 				-- if (tonumber(user.money) >= tonumber(v.withdraw_cost)) then
