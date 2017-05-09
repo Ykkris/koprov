f298109e80312e41427050cf8f304be0f0257e5c
izio38 <romainbillot3009@gmail.com>
2017-05-09 23:38:25 +0200

    Update ichat_server.lua

--- resources/iChat/ichat_server.lua
+++ resources/iChat/ichat_server.lua
@@ -21,6 +21,13 @@ end, function(source, args, user)
 end)
 
 -- ME
+-- TAXI
+TriggerEvent('es:addAdminCommand', 'Taxi', permission.normal, function(source, args, user)
+	table.remove(args, 1)
+	TriggerClientEvent('chatMessage', -1, "^5[911]", {30, 144, 255}, " (^1 Caller ID: ^3" .. GetPlayerName(source) .." | "..source.."^0 ) " .. table.concat(args, " "))
+end, function(source, args, user)
+	TriggerClientEvent('chatMessage', source, "SYSTEM", {255, 0, 0}, "Insufficienct permissions!")
+end)
 TriggerEvent('es:addAdminCommand', 'me', permission.normal, function(source, args, user)
 	table.remove(args, 1)
 	TriggerClientEvent('chatMessage', -1, "^3[Me]", {255, 0, 0}, " (^2 " .. GetPlayerName(source) .." | "..source.."^0 ) " .. table.concat(args, " "))
