c3a02c6ad537d05b1930cf5f9b5b4298bbcfda5e
izio38 <romainbillot3009@gmail.com>
2017-05-09 21:50:07 +0200

    Create pay.lua

---
+++ resources/frfuel/pay.lua
@@ -0,0 +1,18 @@
+database = {
+    host = "127.0.0.1",
+    name = "gta5_gamemode_essential",
+    username = "root",
+    password = "5M32bNCpFdgG"
+}
+
+require "resources/essentialmode/lib/MySQL"
+MySQL:open(database.host, database.name, database.username, database.password)
+
+RegisterServerEvent("essence:payer")
+AddEventHandler("essence:payer", function(litres) -- source, litres en float
+  local prix = litre
+  TriggerEvent('es:getPlayerFromId', source, function(user)
+        user:removeMoney((amount))
+  
+  end)
+end)
