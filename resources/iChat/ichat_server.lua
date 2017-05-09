356861b89f5d1bdb2dda157504060f40d2ac4b9d
izio38 <romainbillot3009@gmail.com>
2017-05-09 23:45:55 +0200

    Update ichat_server.lua

--- resources/iChat/ichat_server.lua
+++ resources/iChat/ichat_server.lua
@@ -1,4 +1,8 @@
-local permission = {
+local Proxy = require("resources/vRP/lib/Proxy")
+
+vRP = Proxy.getInterface("vRP")
+
+ocal permission = {
 	kick = 1,
 	normal = 0,
 	ban = 3
@@ -14,8 +18,12 @@ end)
 
 -- 911 CALL
 TriggerEvent('es:addAdminCommand', '911', permission.normal, function(source, args, user)
+	local userID = vRP.getUserId(source ,function(user_id)
+ 			local = PlayerName
+						end)
+	vRP.getUserIdentity(user_id)
 	table.remove(args, 1)
-	TriggerClientEvent('chatMessage', -1, "^5[911]", {30, 144, 255}, " (^1 Caller ID: ^3" .. GetPlayerName(source) .." | "..source.."^0 ) " .. table.concat(args, " "))
+	TriggerClientEvent('chatMessage', -1, "^5[911]", {30, 144, 255}, " (^1 Caller ID: ^3" .. PlayerName .." | "..source.."^0 ) " .. table.concat(args, " "))
 end, function(source, args, user)
 	TriggerClientEvent('chatMessage', source, "SYSTEM", {255, 0, 0}, "Insufficienct permissions!")
 end)
@@ -23,8 +31,12 @@ end)
 -- ME
 -- TAXI
 TriggerEvent('es:addAdminCommand', 'Taxi', permission.normal, function(source, args, user)
+	local userID = vRP.getUserId(source ,function(user_id)
+ 			local = PlayerName
+						end)
+	vRP.getUserIdentity(user_id)
 	table.remove(args, 1)
-	TriggerClientEvent('chatMessage', -1, "^5[Taxi]", {30, 144, 255}, " (^1 Caller ID: ^3" .. GetPlayerName(source) .." | "..source.."^0 ) " .. table.concat(args, " "))
+	TriggerClientEvent('chatMessage', -1, "^5[Taxi]", {30, 144, 255}, " (^1 Caller ID: ^3" .. PlayerName .." | "..source.."^0 ) " .. table.concat(args, " "))
 end, function(source, args, user)
 	TriggerClientEvent('chatMessage', source, "SYSTEM", {255, 0, 0}, "Insufficienct permissions!")
 end)
