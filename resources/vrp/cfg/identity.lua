08412f257f9a40196900e2398bd3e1b2114fc3b2
Ykkris <nemoselache@gmail.com>
2017-05-10 11:45:55 +0200

    Update vrp

--- resources/vrp/cfg/identity.lua
+++ resources/vrp/cfg/identity.lua
@@ -2,10 +2,10 @@
 local cfg = {}
 
 -- city hall position
-cfg.city_hall = {-425.66708374023, 1123.1030273438, 325.85446166992}
+-- cfg.city_hall = {-425.66708374023, 1123.1030273438, 325.85446166992}
 
 -- cost of a new identity
-cfg.new_identity_cost = 0
+-- cfg.new_identity_cost = 0
 
 -- phone format (max: 20 chars, use D for a random digit)
 cfg.phone_format = "DDD-DDDD"
