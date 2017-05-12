c4405c2453e087804ededb35aed5cbc26f3bbba3
izio38 <romainbillot3009@gmail.com>
2017-05-13 00:09:17 +0200

    Update server.lua

--- resources/[garages job]/taxijob/server.lua
+++ resources/[garages job]/taxijob/server.lua
@@ -1,1 +1,11 @@
 
+
+RegisterServerEvent("taxi:getpaid")
+AddEventHandler("taxi:getpaid", function()
+    -- prendre la source -> user -> user:addMoney()
+  TriggerEvent('es:getPlayerFromId', source, function(user)
+        user:addMoney(25)
+    
+  end)
+  
+end)
