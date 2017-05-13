06f1c12623575c4e0df2be6c6820e6c911f37320
izio38 <romainbillot3009@gmail.com>
2017-05-13 04:19:00 +0200

    Update taxi.lua

--- resources/[garages job]/taxijob/taxi.lua
+++ resources/[garages job]/taxijob/taxi.lua
@@ -319,7 +319,7 @@ Citizen.CreateThread(function()
 	end
 end)
 
-RegisterServerEvent("jobs:yesornot")
+RegisterNetEvent("jobs:yesornot")
 AddEventHandler("jobs:yesornot", function(yesornot)
 	isTaxi = yesornot
 end)
