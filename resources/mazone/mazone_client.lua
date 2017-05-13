10e94b0b97de51883c05b756469cb06543eb9c40
izio38 <romainbillot3009@gmail.com>
2017-05-13 19:20:45 +0200

    Update mazone_client.lua

--- resources/mazone/mazone_client.lua
+++ resources/mazone/mazone_client.lua
@@ -23,7 +23,7 @@ AddEventHandler('mazone:pointage', function()
 	Citizen.Trace("TEST")
 	point = GetEntityCoords(GetPlayerPed(-1))
 	Citizen.Trace(tostring(point.x) .." " .. tostring(point.y))
-	TriggerServerEvent("bugevent", point)
+	TriggerServerEvent("bugevent", table.unpack(point))
 	CancelEvent()
 end)
 
