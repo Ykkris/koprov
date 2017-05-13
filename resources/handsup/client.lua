a5017e49e6ebe072ea2ea93dcc47ee1ec4b86bd3
izio38 <romainbillot3009@gmail.com>
2017-05-13 06:01:05 +0200

    Update client.lua

--- resources/handsup/client.lua
+++ resources/handsup/client.lua
@@ -3,6 +3,7 @@ Citizen.CreateThread(function()
 		Citizen.Wait(0)
 		if IsControlPressed(1, 323) then --Start holding X
 			TaskHandsUp(GetPlayerPed(-1), 1000, GetPlayerPed(-1), -1, true) -- Perform animation.
+			Wait(1000)
 		end
 	end
 end)
