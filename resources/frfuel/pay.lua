7cc69793d004189edaba4cbff417c8657254f189
TwiikerzDev <romain.defossezz@gmail.com>
2017-05-14 15:24:25 +0200

    Medecin paye pas essence (A TESTER)

--- resources/frfuel/pay.lua
+++ resources/frfuel/pay.lua
@@ -8,11 +8,25 @@ database = {
 require "resources/essentialmode/lib/MySQL"
 MySQL:open(database.host, database.name, database.username, database.password)
 
+local isMedic = false 
+
+RegisterNetEvent('medic:receiveIsMedic')
+AddEventHandler('medic:receiveIsMedic', function(result)
+	if(result == "inconnu") then
+		isMedic = false
+	else
+		isMedic = true 
+	end
+end)
+
 RegisterServerEvent("essence:payer")
 AddEventHandler("essence:payer", function(litres) -- source, litres en float
   local prix = math.ceil(litres)
   TriggerEvent('es:getPlayerFromId', source, function(user)
+  	if(isMedic == false) then
         user:removeMoney(prix)
-  
+    else
+    	TriggerClientEvent("es_freeroam:notify", source, "CHAR_BANK_MAZE", 1, "KoprovBank", false, "Facture prise en charge par l'etat !")
+    end  
   end)
 end)
