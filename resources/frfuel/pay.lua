c1ed5aa12ff03606d30d8dfb214c3b2251c50442
izio38 <romainbillot3009@gmail.com>
2017-05-15 06:10:15 +0200

    Update pay.lua

--- resources/frfuel/pay.lua
+++ resources/frfuel/pay.lua
@@ -8,25 +8,37 @@ database = {
 require "resources/essentialmode/lib/MySQL"
 MySQL:open(database.host, database.name, database.username, database.password)
 
-local isMedic = false 
+--local isMedic = false 
 
-RegisterNetEvent('medic:receiveIsMedic')
-AddEventHandler('medic:receiveIsMedic', function(result)
-	if(result == "inconnu") then
-		isMedic = false
-	else
-		isMedic = true 
-	end
-end)
+--RegisterNetEvent('medic:receiveIsMedic')
+--AddEventHandler('medic:receiveIsMedic', function(result)
+--	if(result == "inconnu") then
+--		isMedic = false
+--	else
+--		isMedic = true 
+--	end
+--end)
 
-RegisterServerEvent("essence:payer")
+
+RegisterServerEvent("essence:payer") -- IL FAUDRAIT AUSSI VERIFIER S'IL/ELLE EST EN SERVICE
 AddEventHandler("essence:payer", function(litres) -- source, litres en float
   local prix = math.ceil(litres)
   TriggerEvent('es:getPlayerFromId', source, function(user)
-  	if(isMedic == false) then
-        user:removeMoney(prix)
-    else
-    	TriggerClientEvent("es_freeroam:notify", source, "CHAR_BANK_MAZE", 1, "KoprovBank", false, "Facture prise en charge par l'etat !")
+	local id_medecin = VARIABLE_A_CHANGER
+	local id_policier = VARIABLE_A_CHANGER
+	local player = user.identifier
+    	local req = MySQL:executeQuery("SELECT job FROM users WHERE identifier = '@identifier' ", {['@identifier'] = player })
+    	local resultat = MySQL:getResults(req, {'job'}, "identifier")
+    	if (id_medecin == resultat[1].job) or (id_policier == resultat[1].job)  then
+      		retour = true
+    	else 
+      		retour = false
+     	end
+				
+	if retour then
+		TriggerClientEvent("es_freeroam:notify", source, "CHAR_BANK_MAZE", 1, "KoprovBank", false, "Facture prise en charge par l'etat !")
+	else
+		user:removeMoney(prix)
     end  
   end)
 end)
