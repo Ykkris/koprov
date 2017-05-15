ccb02b91784962915f346b415e6d8e726c7acf0e
izio38 <romainbillot3009@gmail.com>
2017-05-15 06:27:44 +0200

    Update cl_pogarage.lua

--- resources/policegarage/cl_pogarage.lua
+++ resources/policegarage/cl_pogarage.lua
@@ -95,12 +95,15 @@ Citizen.CreateThread(function()
 		DrawMarker(1, 436.516, - 1022.16, 27.662, 0, 0, 0, 0, 0, 0, 2.001, 2.0001, 0.5001, 0, 155, 255, 200, 0, 0, 0, 0)
 		DrawMarker(1, 449.359, - 980.727, 42.60, 0, 0, 0, 0, 0, 0, 2.001, 2.0001, 0.5001, 0, 155, 255, 200, 0, 0, 0, 0)
 		-- Affiche Marqueur pour faire spawn
-		if GetDistanceBetweenCoords(436.516, - 1022.16, 27.662, GetEntityCoords(LocalPed())) < 1 then
+		if GetDistanceBetweenCoords(436.516, - 1022.16, 27.662, GetEntityCoords(LocalPed())) < 2 then
 			drawTxt('Appuyez sur ~g~H~s~ pour faire apparaitre une ~b~voiture', 2, 1, 0.5, 0.8, 0.6, 255, 255, 255, 255)
 			if IsControlJustPressed(1, Keys["H"]) then
 				InitMenuVehicules()
 				Menu.hidden = not Menu.hidden
 			end
+		elseif (GetDistanceBetweenCoords(436.516, -1022.16, 27.662, GetEntityCoords(LocalPed())) > 2 and not Menu.hidden) then
+				Menu.hidden = not Menu.hidden
+		end
 		end
 		if GetDistanceBetweenCoords(449.359, - 980.727, 42.60, GetEntityCoords(LocalPed())) < 2 then
 			drawTxt('Appuyez sur ~g~H~s~ pour faire apparaitre un ~b~Helicopter', 2, 1, 0.5, 0.8, 0.6, 255, 255, 255, 255)
