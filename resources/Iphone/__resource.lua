39cd114075855fb1180960a8e33b4fe2559e7ca2
izio38 <romainbillot3009@gmail.com>
2017-05-15 13:41:56 +0200

    Update __resource.lua

--- resources/Iphone/__resource.lua
+++ resources/Iphone/__resource.lua
@@ -1,12 +1,12 @@
 client_script 'iphone_client.lua'
 -- server_script 'iphone_server.lua'
-ui_page 'client/html/ui.html' --THIS IS IMPORTENT
+ui_page 'html/ui.html' --THIS IS IMPORTENT
 
 --[[The following is for the files which are need for you UI (like, pictures, the HTML file, css and so on) ]]--
 files({
-    'client/html/ui.html',
-    'client/html/scripts.js',
-    'client/html/style.css',
-    'client/html/card.png'
+    'html/ui.html',
+    'html/scripts.js',
+    'html/style.css',
+    'html/card.png'
    -- 'client/html/jquery-3.2.1.min.js'
 })
