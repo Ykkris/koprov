d2d8c171042f2d06b83c82f878c48b560aa6282b
izio38 <romainbillot3009@gmail.com>
2017-05-15 06:23:10 +0200

    N'afk pas les personnes qui sont dans la whitelist (lire le fichier)

--- resources/afkkick/server.lua
+++ resources/afkkick/server.lua
@@ -1,4 +1,22 @@
+-- White-list AFKKICK
+whitelist = {"example1-steamid",
+	"example2-steamid",
+	"example3-steamid"
+	}
+
 RegisterServerEvent("kickForBeingAnAFKDouchebag")
 AddEventHandler("kickForBeingAnAFKDouchebag", function()
-	DropPlayer(source, "Vous êtiez AFK.")
+	afk = true
+	TriggerEvent('es:getPlayerFromId', source, function(user)
+	for i=1, #whitelist do
+		if whitelist[i] == user.identifier then
+			afk = false
+		end
+	end
+				
+	if afk then
+		DropPlayer(source, "Vous êtiez AFK.")
+	end
+					
+	end)
 end)
