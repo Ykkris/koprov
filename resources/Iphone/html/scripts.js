ad7a258beb6cf8c173b91c39f4c1b6673b97f1a5
izio38 <romainbillot3009@gmail.com>
2017-05-15 13:39:59 +0200

    Update scripts.js

--- resources/Iphone/html/scripts.js
+++ resources/Iphone/html/scripts.js
@@ -10,10 +10,11 @@ $(document).ready(function (){
 
         window.addEventListener("message", function(event){
               if (event.data.type == "enableui") {
-                        openMain();
                         $(".tlastname").html(event.data.tlastname);
                         $(".tfirstname").html(event.data.tfirstname);
                         $(".tid").html(event.data.tid);
+                        openMain();
+                        
                 }
                else if (event.data.type == "disableui") {
                         closeMain();
