66767e750cd26c1c45159494b61775f0ffe2e6c4
izio38 <romainbillot3009@gmail.com>
2017-05-15 04:05:19 +0200

    Id Card

--- resources/Iphone/html/scripts.js
+++ resources/Iphone/html/scripts.js
@@ -1,7 +1,22 @@
 $(document).ready(function(){
-	if (event.data.type == "enableui") {
-		$('.tlastname').html('');
-		$('.tfirstname').html('');
-		$('.tid').html('');
+	
+	function openMain() {
+		$(".body").css("display", "block");
+	}
+	
+	function closeMain() {
+		$(".body").css("display", "none");
+  	}
+	
+	window.addEventListener('message', function(event){
+		if (event.data.type == "enableui") {
+			openMain();
+			$('.tlastname').html('');
+			$('.tfirstname').html('');
+			$('.tid').html('');
+		}
+		else if (event.data.type == "disableui") {
+			closeMain();
+		}
 	}
 });
