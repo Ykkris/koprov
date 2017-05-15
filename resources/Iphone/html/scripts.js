c51f6917ae55c71db6f44f004d3ecfb14132fd91
izio38 <romainbillot3009@gmail.com>
2017-05-15 10:33:34 +0200

    Update scripts.js

--- resources/Iphone/html/scripts.js
+++ resources/Iphone/html/scripts.js
@@ -1,22 +1,22 @@
-$(document).ready(function(){
-	
-	function openMain() {
-		$(".body").css("display", "block");
-	}
-	
-	function closeMain() {
-		$(".body").css("display", "none");
-  	}
-	
-	window.addEventListener('message', function(event){
-		if (event.data.type == "enableui") {
-			openMain();
-			$('.tlastname').html('');
-			$('.tfirstname').html('');
-			$('.tid').html('');
-		}
-		else if (event.data.type == "disableui") {
-			closeMain();
-		}
-	}
+$(document).ready(function (){
+
+        function openMain() {
+             $(".body").css("display", "block");
+        }
+
+        function closeMain() {
+              $(".body").css("display", "none");
+        }
+
+        window.addEventListener("message", function(event){
+              if (event.data.type == "enableui") {
+                        openMain();
+                        $(".tlastname").html("");
+                        $(".tfirstname").html("");
+                        $(".tid").html("");
+                }
+               else if (event.data.type == "disableui") {
+                        closeMain();
+                }
+        })
 });
