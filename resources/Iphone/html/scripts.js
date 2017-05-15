6dbccb6a37283dd65b71971de740b6f061d543ff
izio38 <romainbillot3009@gmail.com>
2017-05-15 13:20:48 +0200

    Update scripts.js

--- resources/Iphone/html/scripts.js
+++ resources/Iphone/html/scripts.js
@@ -1,7 +1,7 @@
 $(document).ready(function (){
 
         function openMain() {
-             $(".body").css("display", "block");
+             $(".body").css("display", "container");
         }
 
         function closeMain() {
