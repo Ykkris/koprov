1d83f71539da4785487dc6ae6aad17110e7c4736
Ykkris <nemoselache@gmail.com>
2017-05-16 19:33:41 +0200

    PUSH PUSH

--- resources/four/__resource.lua
+++ resources/four/__resource.lua
@@ -2,5 +2,5 @@
 --resource_manifest_version '77731fab-63ca-442c-a67b-abc70f28dfa5'
 
 -- General
-client_script 'client.lua'
-server_script 'server.lua'
+client_script 'four_client.lua'
+server_script 'four_server.lua'
