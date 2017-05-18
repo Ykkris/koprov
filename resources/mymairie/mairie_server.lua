997bed0e888afcb197e3060786f33ff9740c4116
izio38 <romainbillot3009@gmail.com>
2017-05-19 01:23:16 +0200

    Update mairie_server.lua

--- resources/mymairie/mairie_server.lua
+++ resources/mymairie/mairie_server.lua
@@ -3,10 +3,12 @@ MySQL:open("127.0.0.1", "gta5_gamemode_essential", "root", "5M32bNCpFdgG")
 
 RegisterServerEvent("mymairie:identity")
 AddEventHandler("mymairie:identity",function(prenom, nom, age)
-	RconPrint(tostring(prenom))
-	RconPrint(tostring(nom))
-	RconPrint(tostring(age))
+	TriggerEvent('es:getPlayerFromId', source, function(user)
+	--RconPrint(tostring(prenom))
+	--RconPrint(tostring(nom))
+	--RconPrint(tostring(age))
 
-	local executed_query = MySQL:executeQuery("UPDATE users (`first_name`, `last_name`, `age`) VALUES ('@prenom', '@nom', '@age')",
-												{['@prenom'] = prenom, ['@nom'] = nom, ['@age'] = age})
-end)
+	local executed_query = MySQL:executeQuery("UPDATE users (`first_name`, `last_name`, `age`) VALUES ('@prenom', '@nom', '@age') WHERE identifier='@identifier'",
+												{['@prenom'] = prenom, ['@nom'] = nom, ['@age'] = age, ['@identifier'] = user.identifier})
+	end
+			end)
