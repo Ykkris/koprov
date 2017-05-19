033ea136d83be99132ce515a5690d8c206a2d542
Ykkris <nemoselache@gmail.com>
2017-05-19 05:05:34 +0200

    Parenthèse mairie

--- resources/mymairie/mairie_server.lua
+++ resources/mymairie/mairie_server.lua
@@ -9,7 +9,7 @@ AddEventHandler("mymairie:identity",function(prenom, nom, age)
 	RconPrint(tostring(age))
 
 	MySQL:executeQuery("UPDATE users SET last_name = '@last_name', first_name = '@first_name', age = '@age' WHERE identifier = '@identifier' ",
-					{['@last_name'] = nom , ['@first_name'] = prenom , ['@age'] = age, ['@identifier'] = user.identifier)}
+					{['@last_name'] = nom , ['@first_name'] = prenom , ['@age'] = age, ['@identifier'] = user.identifier})
 				
 	end)
 		
