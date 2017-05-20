96473558e1f1e6b966b04e8123eed1a693e170c2
izio38 <romainbillot3009@gmail.com>
2017-05-20 12:35:17 +0200

    Blanchissement (sans debug)

--- resources/blanchissement/client/client_main_blanchissement.lua
+++ resources/blanchissement/client/client_main_blanchissement.lua
@@ -4,40 +4,99 @@
 -- Version: v0.0.1
 -- Path: client/main.lua
 ------
+
+RegisterNetEvent("blanchissement:notification")
+RegisterNetEvent("blanchissement:receiveblanchissement")
+RegisterNetEvent("blanchissement:mission")
+
+-------------------------- HARDCORDIN BE PREPARED ----------------------------
+Keys = {
+	["ESC"] = 322, ["F1"] = 288, ["F2"] = 289, ["F3"] = 170, ["F5"] = 166, ["F6"] = 167, ["F7"] = 168, ["F8"] = 169, ["F9"] = 56, ["F10"] = 57, 
+	["~"] = 243, ["1"] = 157, ["2"] = 158, ["3"] = 160, ["4"] = 164, ["5"] = 165, ["6"] = 159, ["7"] = 161, ["8"] = 162, ["9"] = 163, ["-"] = 84, ["="] = 83, ["BACKSPACE"] = 177, 
+	["TAB"] = 37, ["Q"] = 44, ["W"] = 32, ["E"] = 38, ["R"] = 45, ["T"] = 245, ["Y"] = 246, ["U"] = 303, ["P"] = 199, ["["] = 39, ["]"] = 40, ["ENTER"] = 18,
+	["CAPS"] = 137, ["A"] = 34, ["S"] = 8, ["D"] = 9, ["F"] = 23, ["G"] = 47, ["H"] = 74, ["K"] = 311, ["L"] = 182,
+	["LEFTSHIFT"] = 21, ["Z"] = 20, ["X"] = 73, ["C"] = 26, ["V"] = 0, ["B"] = 29, ["N"] = 249, ["M"] = 244, [","] = 82, ["."] = 81,
+	["LEFTCTRL"] = 36, ["LEFTALT"] = 19, ["SPACE"] = 22, ["RIGHTCTRL"] = 70, 
+	["HOME"] = 213, ["PAGEUP"] = 10, ["PAGEDOWN"] = 11, ["DELETE"] = 178,
+	["LEFT"] = 174, ["RIGHT"] = 175, ["TOP"] = 27, ["DOWN"] = 173,
+	["NENTER"] = 201, ["N4"] = 108, ["N5"] = 60, ["N6"] = 107, ["N+"] = 96, ["N-"] = 97, ["N7"] = 117, ["N8"] = 61, ["N9"] = 118
+}
+--------------------------------------------------------------------------------
+
 zoneBlanchissement = {x = 3597.546, y = 3678.030  , z = 39.808}
+zoneRecuperation = {
+	{x = , y = , z = },
+	{x = , y = , z = },
+	{x = , y = , z = }
+}
 radius = 100.0
 isPed = false
+sommeMini = 50
+alreadySend = false
 
-
+soulMessage = {"Laisse moi fumer ma beuh man...", "J'suis trop hype, j'peux plus rien pour toi là", "Tu vas me foutre la paix ?.. J'espère qu'elle est grosse la paix", "Dégage de ma vue",
+"Tu vas pas me souler toute la journée quand meme ?!", "Un jour, ya un type qui m'a dit que Romain fumait beaucoup trop, j'sais pas perso, on a pas la même visions des choses peut-être",
+"On m'a dit que je mettai des H au mot Wait... Foutaise !", "Je rigole bien quand je vois ta sale tronche quand même"}
 
 Citizen.CreateThread(function()
+
 	Citizen.Wait(1000) -- Pour éviter de faire surcharger les données
 
-	local isPlayerNear = IsNear(GetPlayerPed(-1))
+	local isPlayerNear = IsNearPedFromPointWithRadius(GetPlayerPed(-1), zoneBlanchissement.x, zoneBlanchissement.y, zoneBlanchissement.z, 100.0)
+
 	if isPlayerNear then -- Donc s'il est bien à côté alors :
+		Citizen.Trace(tostring(isTrevor))
 		isPed = IsSpecificPedHashNearPed(GetPlayerPed(-1), GetHashKey( "a_f_y_hippie_01" ))
 		if not(isPed) then
-				RequestModel( GetHashKey( "a_f_y_hippie_01" ) )
-				hash = GetHashKey( "a_f_y_hippie_01" )
+			local ped = GetClosestPed(coords, 20.0, 1, 0, 0, 0, -1)
+			if not(isTrevor) then
+				
+				blanchisseuse = CreatePedWithHashAtCoordsAndReturn("a_f_y_hippie_01", zoneBlanchissement.x, zoneBlanchissement.y, zoneBlanchissement.z)
 
-				while not HasModelLoaded(hash) do -- On attend qu'il finisse de charger le modèle
-			    	Wait(1000)
-			    	Citizen.Trace("PAS CHARGE")
-				end
-
-				Blanchisseur = CreatePed(5, hash, zoneBlanchissement.x, zoneBlanchissement.y, zoneBlanchissement.y, 0.0 ,true)
-		        SetEntityAsMissionEntity(Blanchisseur, true, true)
-		        SetEntityInvincible(Blanchisseur, true)
-		        SetEntityProofs(Blanchisseur, 1, 1, 1, 1, 1, 1, 1, 1)
+		        SetEntityAsMissionEntity(blanchisseuse, true, true)
+		        SetEntityInvincible(blanchisseuse, true)
+		        SetEntityProofs(blanchisseuse, 1, 1, 1, 1, 1, 1, 1, 1)
 
 		        -- GetClosestPed(x, y, z, radius, p4, p5, outPed, p7, p8, pedType)
 	    	end
     	end
 
+    	if IsNearPedFromPointWithRadius(GetPlayerPed(-1), zoneBlanchissement.x, zoneBlanchissement.y, zoneBlanchissement.z, 15.0) then
+    		DrawMissionText("Vous entendez : 'Wouah ! Comment elle est bonne cette beuh !'", 3000)
+	    	if IsControlJustPressed(1, Keys['E']) and IsNearPedFromPointWithRadius(GetPlayerPed(-1), zoneBlanchissement.x, zoneBlanchissement.y, zoneBlanchissement.z, 3.0) and not(alreadySend) then
+
+	    		TriggerServerEvent("blanchissement:sendblanchissement")
+
+	    		while not(received) do
+	    			Wait(1000)
+	    		end
+
+	    		AddEventHandler("blanchissement:receiveblanchissement", function(argent)
+	    			received = true
+	    			if argent >= sommeMini then
+		    			DrawMissionText("Envois ton numéro gringos, le chef te tiens au courant. Ne rate pas le rendez vous sinon on garde tout.", 5000)
+		    			Citizen.Wait(5000)
+		    			DrawMissionText("Allez, tires toi maintenant, j'ai d'autres choses à faire.", 4000)
+		    			alreadySend = true
+		    		else
+		    			DrawMissionText("Tu veux que je fasse quoi avec ça ? *rifougne*", 4000)
+		    		end
+
+	    		end)
+
+	    	elseif IsControlJustPressed(1, Keys['E']) and IsNearPedFromPointWithRadius(GetPlayerPed(-1), zoneBlanchissement.x, zoneBlanchissement.y, zoneBlanchissement.z, 3.0) and alreadySend then
+	    		local message = math.random(1, #soulMessage)
+	    		DrawMissionText(soulMessage[message], 5000)
+	    		Citizen.Wait(5000)
+	    	end
+
+	    	end
+	    end
+	end
 
 end)
 
-function IsNear(player) -- on créer une fonction auxilliaire qu'on pourra ré-utiliser
+function IsNear(player, point, radius) -- on créer une fonction auxilliaire qu'on pourra ré-utiliser
 
 	local playerCoords =  GetEntityCoords(player, true) -- true pour alive, sinon false qui renvois un Vecteur3 : c'est un array
 	local distance = GetDistanceBetweenCoords(playerCoords.x, playerCoords.y, playerCoords.z, zoneBlanchissement.x, zoneBlanchissement.y, zoneBlanchissement.z , true) -- onrécupère la distance entre le joueur et le point de blanchissement
@@ -67,6 +126,93 @@ function IsSpecificPedHashNearPed(player, modelhash)
 
 end
 
-function CreatePedWithHashAtCoordsAndReturn(hash)
-	
+function CreatePedWithHashAtCoordsAndReturn(model, x, y, z)
+    RequestModel( GetHashKey( model) )
+    hash = GetHashKey( model)
+    local i = 0
+    while not HasModelLoaded(hash) and i <= 5 do -- On attend qu'il finisse de charger le modèle
+        Wait(200)
+        i = i+1
+    end
+
+    if i == 5 then
+        return "Error : Hash not loaded"
+    else
+        local Ped = CreatePed(5, hash, x, y, z, 0.0 ,true)
+    end
+
+    return Ped
 end
+
+function DrawMissionText(m_text, showtime)
+          ClearPrints()
+          SetTextEntry_2("STRING")
+          AddTextComponentString(m_text)
+          DrawSubtitleTimed(showtime, 1)
+end
+
+
+AddEventHandler("blanchissement:notification", function(text)
+	DrawMissionText(text, 4000)
+
+end)
+
+AddEventHandler("blanchissement:mission", function()  -- le joueur à été séléctionné par le serveur
+	local time = os.clock()
+	DrawMissionText("~HUD_COLOUR_VIDEO_EDITOR_SCORE~ Dépêche toi de venir, je t'envois les données GPS. Dans 15 minutes j'me casse avec le sac.", 20000)
+	lcoal random = maths.random(1 , 3)
+	local zoneRecupRandom = zoneRecuperation[random]
+
+	Blipvariable = AddBlipForCoord(zoneRecupRandom.x , zoneRecupRandom.y , zoneRecupRandom.z)
+	N_0x80ead8e2e1d5d52e(Blipvariable)
+	SetBlipRoute(Blipvariable, 1)
+	SetBlipRouteColour(Blipvariable, 59)
+
+	Citizen.CreateThread(function()
+
+
+		if isNearArea(zoneRecupRandom.x , zoneRecupRandom.y , zoneRecupRandom.z) then
+			sac = CreateObject(modelHash, zoneRecupRandom.x, zoneRecupRandom.y, zoneRecupRandom.z, true, 1, 1)
+			PlaceObjectOnGroundProperly(sac)
+
+			RequestModel(GetHashKey("prop_money_bag_01"))
+			while HasModelLoaded(GetHashKey("prop_money_bag_01")) do
+				Wait(10)
+			end
+
+			sac = CreateAmbientPickup(GetHashKey("PICKUP_MONEY_MED_BAG"), zoneRecupRandom.x, zoneRecupRandom.y, zoneRecupRandom.z, 0, 0, GetHashKey("prop_money_bag_01"), 0, 1)
+			
+			--CreatePickup(GetHashKey('prop_money_bag_01'), location.x, location.y, location.z)
+			while not(HasPickupBeenCollected(sac)) or ((os.clock() - time) > 900) do
+				Wait(10)
+			end
+
+			if ((os.clock() - time) > 900 then
+				DrawMissionText("~HUD_COLOUR_VIDEO_EDITOR_SCORE~ Trop tard, j'me tire avec le sac.", 10000)
+			else
+				TriggerServerEvent("mission:sendmoney")
+			end
+
+			if Blipvariable ~= nil and DoesBlipExist(Blipvariable) then
+				Citizen.InvokeNative(0x86A652570E5F25DD,Citizen.PointerValueIntInitialized(Blipvariable))
+				Blipvariable = nil
+			end
+		end
+	end)
+end)
+
+function isNearArea(x,y,z,radius) -- Je ne vais pas utiliser le z finalement, à voir
+	local coords_player = GetEntityCoords(GetPlayerPed(-1), true)
+	local x2 = coords_player.x
+	local y2 = coords_player.y
+	local z2 = coords_player.z
+	local dist = GetDistanceBetweenCoords(x, y, z, x2, y2, z2, false)
+	
+
+	if dist > radius then
+		return false
+	else
+		return true
+	end
+end
+
--- resources/blanchissement/server/server_main_blanchissement.lua
+++ resources/blanchissement/server/server_main_blanchissement.lua
@@ -5,3 +5,169 @@
 -- Path: server/main.lua
 ------
 
+database = {
+    host = "127.0.0.1",
+    name = "gta5_gamemode_essential",
+    username = "root",
+    password = "5M32bNCpFdgG"
+}
+
+require "resources/essentialmode/lib/MySQL"
+MySQL:open(database.host, database.name, database.username, database.password)
+
+tempsEntreChaqueMission = 2400000 -- 40 minutes (en ms)
+tempsEntreLeDepotEtLaPaye = 10800 -- 3 h (en secondes)
+
+AddEventHandler("onResourceStart", function(resource)
+	if resource == "blanchissement" then
+		BlanchissementRandom()
+	end
+end)
+
+RegisterServerEvent("mission:sendmoney")
+AddEventHandler("mission:sendmoney", function()
+	TriggerEvent("getPlayerFromId", source, funtion(user)
+		local req = MySQL:executeQuery("SELECT dirty_money FROM users WHERE identifier = '@identifier' ", {['@identifier'] = user.identifier })
+		
+	    local resultat = MySQL:getResults(req, {'dirty_money'}, "identifier")
+		money = tonumber(resultat[1].dirty_money)
+		local rargent
+	  	policier = NombrePolicier()
+	  	local Policier = {0.65, 0.7, 0.85, 0.90, 1}
+
+	 	if policier > 5 then 
+	 		rargent = money
+	 	else
+	 		rargent = money * Policier[nombrePolicier]
+	 	end
+
+	 	user:addMoney(rargent)
+	 	TriggerClientEvent("blanchissement:notification", source, "Tiens, allez casse toi p'tite merde !")
+	 end)
+
+end)
+
+
+RegisterServerEvent("blanchissement:sendblanchissement")
+AddEventHAndler("blanchissement:sendblanchissement", function()
+
+	TriggerEvent("getPlayerFromId", source,function(user)
+
+		local req = MySQL:executeQuery("SELECT dirty_money FROM users WHERE identifier = '@identifier' ", {['@identifier'] = user.identifier })
+    	local resultat = MySQL:getResults(req, {'dirty_money'}, "identifier")
+
+    	local argent = tonumber(resultat[1].dirty_money)
+
+    	local receiveMoney = CalculMoney(argent)
+
+    	local time = os.time()
+
+    	MySQL:executeQuery("UPDATE users SET dirty_time = '@dirty_time', dirty_wait_money = '@dirty_wait_money' WHERE identifier = '@identifier' ",
+					{['@dirty_time'] = time , ['@dirty_wait_money'] = receiveMoney,['@identifier'] = user.identifier})
+
+
+
+	end)
+
+
+end)
+
+function CalculMoney(bargent)
+	local Pallier = {50, 60, 65, 70, 80}
+	local pallier = 0
+	--local Policier = {0.65, 0.7, 0.85, 0.90, 1}
+
+
+	--local nombrePolicier = NombrePolicier()
+
+	if bargent < 950 then
+		pallier = 1
+	elseif bargent < 950 * 3 then
+		pallier = 2
+	elseif bargent < 950 * 10 then
+		pallier = 3
+	elseif bargent < 950 * 25 then
+		pallier = 4
+	else
+		pallier = 5
+	end
+
+	local multiplicateur = maths.random(Pallier[pallier], Pallier[pallier+1])
+
+	--if nombrePolicier == 0 then
+	return (bargent * multiplicateur)/100) --* 0.5
+	--else
+		--return (((bargent * multiplicateur)/100) * Policier[nombrePolicier])
+	--end
+end
+
+function BlanchissementRandom()
+	isInWait = false
+	isWaitingForLong = false
+	found = false
+
+	TriggerEvent("es:getPlayers", function(Users)
+		for k,v in pairs(Users) do
+
+			sargent = IsInWait(Users[k])
+			if sargent ~= 0 then
+				if IsWaitingForLong(Users[k]) then
+					found = true
+					TriggerClientEvent("blanchissement:mission", k, sargent)
+				end
+			end
+			if found then
+				break
+			end
+
+		end
+	end)
+	SetTimeout(tempsEntreChaqueMission, BlanchissementRandom())
+end
+
+function IsInWait(player)
+
+	local req = MySQL:executeQuery("SELECT dirty_wait_money FROM users WHERE identifier = '@identifier' ", {['@identifier'] = player.identifier })
+    local resultat = MySQL:getResults(req, {'dirty_wait_money'}, "identifier")
+
+    if result[1].dirty_wait_money == 0 then
+    	return 0
+    else 
+    	return tonumber(result[1].dirty_wait_money)
+    end
+
+end
+
+function IsWaitingForLong(player)
+	time = os.clock()
+
+	local req = MySQL:executeQuery("SELECT dirty_time FROM users WHERE identifier = '@identifier' ", {['@identifier'] = player.identifier })
+    local resultat = MySQL:getResults(req, {'dirty_time'}, "identifier")
+
+    if (resultat[1].dirty_time - time) >= tempsEntreLeDepotEtLaPaye then
+    	return true
+    else
+    	return false
+    end
+
+end
+
+function NombrePolicier()
+	local policier = 0
+	TriggerEvent("es:getPlayers", function(Users)
+		if Users==nil then return 0
+		else --2
+			for k,v in pairs(Users) -- Cela peut être optimisée en récupérant tous les identifier dont le job est 2 puis de faire une boucle qui va vérifier en interne (une seule requete au lieu de 1 jusqu'a 24) mais elle ne va se faire qu'une fois toutes les 20 minutes alors balek
+				local req = MySQL:executeQuery("SELECT job FROM users WHERE identifier = '@identifier' ", {['@identifier'] = Users[k].identifier })
+	    		local resultat = MySQL:getResults(req, {'job'}, "identifier")
+	    		if resultat[1].job == 2 then
+	    			policier = policier + 1
+	    		end
+	    	end
+
+
+		end
+
+	end)
+	return policier
+end
