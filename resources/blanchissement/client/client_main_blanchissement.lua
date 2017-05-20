ab4bebbfde8565669df44db37268bd24d08ff14b
izio38 <romainbillot3009@gmail.com>
2017-05-20 06:18:41 +0200

    Update client_main_blanchissement.lua

--- resources/blanchissement/client/client_main_blanchissement.lua
+++ resources/blanchissement/client/client_main_blanchissement.lua
@@ -6,44 +6,43 @@
 ------
 zoneBlanchissement = {x = 3597.546, y = 3678.030  , z = 39.808}
 radius = 100.0
-isTrevor = false
+isPed = false
 
 
 
 Citizen.CreateThread(function()
 	Citizen.Wait(1000) -- Pour éviter de faire surcharger les données
 
-	local isPlayerNear = isNear(GetPlayerPed(-1))
+	local isPlayerNear = IsNear(GetPlayerPed(-1))
+	Citizen.Trace(tostring(isPlayerNear)
 	if isPlayerNear then -- Donc s'il est bien à côté alors :
+		Citizen.Trace(tostring(isTrevor))
+		isPed = IsSpecificPedHashNearPed(GetPlayerPed(-1), GetHashKey( "a_f_y_hippie_01" ))
+		if not(isPed) then
+			local ped = GetClosestPed(coords, 20.0, 1, 0, 0, 0, -1)
+			if not(isTrevor) then
+				RequestModel( GetHashKey( "a_f_y_hippie_01" ) )
+				hash = GetHashKey( "a_f_y_hippie_01" )
 
-		outped, t = GetClosestPed(zoneBlanchissement.x, zoneBlanchissement.y, zoneBlanchissement.z, radius, 0, 0, 0, 0, 5)
-		Citizen.Trace(tostring(outped))
-		Citizen.Trace(tostring(t))
+				while not HasModelLoaded(hash) do -- On attend qu'il finisse de charger le modèle
+			    	Wait(1000)
+			    	Citizen.Trace("PAS CHARGE")
+				end
 
-		if not(isTrevor) then
-			RequestModel( GetHashKey( "a_f_y_hippie_01" ) )
-			hash = GetHashKey( "a_f_y_hippie_01" )
+				Blanchisseur = CreatePed(5, hash, zoneBlanchissement.x, zoneBlanchissement.y, zoneBlanchissement.y, 0.0 ,true)
+		        SetEntityAsMissionEntity(Blanchisseur, true, true)
+		        SetEntityInvincible(Blanchisseur, true)
+		        SetEntityProofs(Blanchisseur, 1, 1, 1, 1, 1, 1, 1, 1)
 
-			while not HasModelLoaded(hash) do -- On attend qu'il finisse de charger le modèle
-		    	Wait(1000)
-			end
-
-			Blanchisseur = CreatePed(5, hash, zoneBlanchissement.x, zoneBlanchissement.y, zoneBlanchissement.y, 0.0 ,true)
-	        SetEntityAsMissionEntity(Blanchisseur, true, true)
-	        SetEntityInvincible(Blanchisseur, true)
-				
-			outped, t = GetClosestPed(zoneBlanchissement.x, zoneBlanchissement.y, zoneBlanchissement.z, radius, 0, 0, 0, 0, 5)
-		Citizen.Trace(tostring(outped))
-		Citizen.Trace(tostring(t))
-
-	        -- GetClosestPed(x, y, z, radius, p4, p5, outPed, p7, p8, pedType)
+		        -- GetClosestPed(x, y, z, radius, p4, p5, outPed, p7, p8, pedType)
+	    	end
     	end
 
 	end
 
 end)
 
-function isNear(player) -- on créer une fonction auxilliaire qu'on pourra ré-utiliser
+function IsNear(player) -- on créer une fonction auxilliaire qu'on pourra ré-utiliser
 
 	local playerCoords =  GetEntityCoords(player, true) -- true pour alive, sinon false qui renvois un Vecteur3 : c'est un array
 	local distance = GetDistanceBetweenCoords(playerCoords.x, playerCoords.y, playerCoords.z, zoneBlanchissement.x, zoneBlanchissement.y, zoneBlanchissement.z , true) -- onrécupère la distance entre le joueur et le point de blanchissement
@@ -54,3 +53,21 @@ function isNear(player) -- on créer une fonction auxilliaire qu'on pourra ré-u
 		return false
 	end
 end 
+
+function IsSpecificPedHashNearPed(player, modelhash)
+	local playerCoords = GetEntityCoords(player, true)
+	local ped = GetClosestPed(playerCoords.x, playerCoords.y, playerCoords.z, radius, 1, 0, 0, 0, -1)
+
+	if ped == nil then
+		return false
+	end
+
+	local isSpecificPed = IsPedModel(ped, modelhash)
+
+	if isSpecificPed then
+		return true
+	else
+		return false
+	end
+
+end
