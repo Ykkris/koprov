98c744e21601afa7c14de435aa4fc7791aa32403
TwiikerzDev <romain.defossezz@gmail.com>
2017-05-25 12:55:17 +0200

    PEUSH J'AI DIT PEUUSHH

--- resources/frfuel/pay.lua
+++ resources/frfuel/pay.lua
@@ -25,23 +25,24 @@ AddEventHandler("essence:payer", function(litres) -- source, litres en float
   local prix = math.ceil(litres)
   TriggerEvent('police:checkService', source, function(inserv)
   TriggerEvent('es:getPlayerFromId', source, function(user)
-	local id_medecin = 3
-	local id_policier = 2
-	local player = user.identifier
-  local copinservice = inserv
-    	local req = MySQL:executeQuery("SELECT job FROM users WHERE identifier = '@identifier' ", {['@identifier'] = player })
-    	local resultat = MySQL:getResults(req, {'job'}, "identifier")
-    	if (3 == resultat[1].job) or (2 == resultat[1].job) and (copinservice)  then
-      		retour = true
-    	else 
-      		retour = false
-     	end
+	  local id_medecin = 3
+	  local id_policier = 2
+	  local player = user.identifier
+    local copinservice = inserv
+    local req = MySQL:executeQuery("SELECT job FROM users WHERE identifier = '@identifier' ", {['@identifier'] = player })
+    local resultat = MySQL:getResults(req, {'job'}, "identifier")
+    if (3 == resultat[1].job) or (2 == resultat[1].job) and (copinservice)  then
+      retour = true
+    else 
+      retour = false
+    end
 				
-	if retour then
+	 if retour then
 		--TriggerClientEvent("es_freeroam:notify", source, "CHAR_BANK_MAZE", 1, "KoprovBank", false, "Facture prise en charge par l'etat !")
     TriggerClientEvent("pNotify:SendNotification", source, { text = "Facture de <b style='color:green'>".. prix.."$</b> prise en charge pas l'état !", type = "info", timeout = 10000, layout = "centerLeft",})
-	else
+	 else
 		user:removeMoney(prix)
-    end  
+  end  
   end)
+end)
 end)
