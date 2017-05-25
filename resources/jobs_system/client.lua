4b9c9f7c13f64252dd91f8e4597684e8e590bc7e
Ykkris <nemoselache@gmail.com>
2017-05-25 15:36:03 +0200

    Fermier

--- resources/jobs_system/client.lua
+++ resources/jobs_system/client.lua
@@ -7,7 +7,7 @@ local changeYourJob = {
 local jobs = {
   {name="Dépanneur", id=4},
   {name="Mineur", id=5},
-  {name="Trafiquant", id=6},
+  {name="Fermier", id=10},
   {name="Bûcheron", id=7},
   {name="Pêcheur", id=8},
   {name="Taxis", id=9}
