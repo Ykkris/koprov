7c4c9cc550646e707b307d17f068b6c3b2fea6f3
Twiikerz <mehdicraftdiamond@gmail.com>
2017-05-31 10:22:27 +0200

    Revert 2 last commits

--- resources/ply_garages/client.lua
+++ resources/ply_garages/client.lua
@@ -246,10 +246,8 @@ AddEventHandler('ply_garages:SpawnVehicle', function(vehicle, plate, state, prim
 				TaskWarpPedIntoVehicle(playerPed, veh, -1)
 				if GetDistanceBetweenCoords(405.175, -1642.475, 28.295, GetEntityCoords(LocalPed())) < 10 then
 					TriggerServerEvent('garages:ToPay')
-					TriggerServerEvent('ply_garages:SetVehOutOfFour', vehicle, plate)
-				else 				
+				end 				
 				TriggerServerEvent('ply_garages:SetVehOut', vehicle, plate)
-				end
    				TriggerServerEvent("ply_garages:CheckGarageForVeh")
 			end
 		end
