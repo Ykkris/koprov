d5b222085262abe43450f7b5394927a75a23c34f
Ykkris <nemoselache@gmail.com>
2017-06-06 11:10:50 +0200

    Update thecops

--- resources/thecops/__resource.lua
+++ resources/thecops/__resource.lua
@@ -1,19 +1,19 @@
 -- Manifest
 resource_manifest_version '77731fab-63ca-442c-a67b-abc70f28dfa5'
 
--- Requiring essentialmode
-dependency 'essentialmode'
-
 -- General
 client_scripts {
-  'thecops_client.lua',
+  'config.lua',
+  'client.lua',
   'vestpolice.lua',
   'menupolice.lua',
   'policeveh.lua',
 }
 
 server_scripts {
-  'thecops_server.lua'
+  'config.lua',
+  'config_db.lua',
+  'server.lua',
 }
 
 export 'getIsInService'
